aab60aed898c7611f0b0f009e9e568756865822d
mahbub.pasha <mahbub.pasha@randomaccessinc.com>
2022-12-17 20:00:39 +0600

    edit exercise

--- lib/blocs/workouts_cubit.dart
+++ lib/blocs/workouts_cubit.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sociallogin/models/exercise.dart';
 import 'package:sociallogin/models/workout.dart';
 
 class WorkoutsCubit extends Cubit<List<Workout>>{
@@ -15,5 +16,29 @@ class WorkoutsCubit extends Cubit<List<Workout>>{
       workouts.add(Workout.fromJson(el));
     }
     emit(workouts);
+  }
+  
+  saveWorkout(Workout workout, int index){
+    Workout newWorkout = Workout(title: workout.title, exercises: []);
+    int exIndex = 0;
+    int startTime = 0;
+    
+    for(var ex in workout.exercises!){
+      newWorkout.exercises!.add(
+        Exercise(
+          title: ex.title,
+          prelude: ex.prelude,
+          duration: ex.duration,
+          index: ex.index,
+          startTime: ex.startTime
+        ),
+      );
+      exIndex++;
+      startTime += ex.prelude! + ex.duration!;
+    }
+    state[index] = newWorkout;
+    print("...I've  ${state.length} states");
+    ///... means get everything in the list
+    emit([...state]);
   }
 }
--- lib/screens/edit_exercise_screen.dart
+++ lib/screens/edit_exercise_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:sociallogin/blocs/workouts_cubit.dart';
+import 'package:sociallogin/helpers.dart';
 import 'package:sociallogin/models/workout.dart';
 
 class EditExerciseScreen extends StatefulWidget {
@@ -34,12 +39,137 @@ class _EditExerciseScreenState extends State<EditExerciseScreen> {
         Row(
           children: [
             Expanded(
+              child: InkWell(
+                onLongPress: ()=>showDialog(
+                  context: context,
+                  builder: (_){
+                    final controller = TextEditingController(
+                      text: widget.workout!.exercises![widget.exIndex!].prelude!.toString()
+                    );
+                    return AlertDialog(
+                      content: TextField(
+                        controller: controller,
+                        decoration: const InputDecoration(
+                          labelText: "Prelude (seconds)",
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: (){
+                            if(controller.text.isNotEmpty){
+                              Navigator.pop(context);
+                              setState(() {
+                                /// copy the new value or existing value of prelude
+                                widget.workout!.exercises![widget.exIndex!]
+                                = widget.workout!.exercises![widget.exIndex!].copyWith(
+                                    prelude: int.parse(controller.text)
+                                );
+                                /// store all values into cubit
+                                BlocProvider.of<WorkoutsCubit>(context).saveWorkout(widget.workout!, widget.index);
+                              });
+                            }
+                          },
+                          child: const Text("Save"),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                child: NumberPicker(
+                  itemHeight: 30.0,
+                  value: widget.workout!.exercises![widget.exIndex!].prelude!,
+                  minValue: 0,
+                  maxValue: 3599,
+                  textMapper: (strVal)=>formatTime(int.parse(strVal), false),
+                  onChanged: (value)=>setState(() {
+                    /// copy the new value or existing value of prelude
+                    widget.workout!.exercises![widget.exIndex!]
+                    = widget.workout!.exercises![widget.exIndex!].copyWith(
+                        prelude: value
+                    );
+                    /// store all values into cubit
+                    BlocProvider.of<WorkoutsCubit>(context).saveWorkout(widget.workout!, widget.index);
+                  }),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
               child: TextField(
                 textAlign: TextAlign.center,
                 controller: _title,
                 onChanged: (value)=>setState(() {
-
+                  /// copy the new value or existing value of title
+                  widget.workout!.exercises![widget.exIndex!]
+                  = widget.workout!.exercises![widget.exIndex!].copyWith(
+                    title: value
+                  );
+                  /// store all values into cubit
+                  BlocProvider.of<WorkoutsCubit>(context).saveWorkout(widget.workout!, widget.index);
                 }),
+              ),
+            ),
+            Expanded(
+              child: InkWell(
+                onLongPress: ()=>showDialog(
+                  context: context,
+                  builder: (_){
+                    final controller = TextEditingController(
+                        text: widget.workout!.exercises![widget.exIndex!].duration!.toString()
+                    );
+                    return AlertDialog(
+                      content: TextField(
+                        controller: controller,
+                        decoration: const InputDecoration(
+                          labelText: "Duration (seconds)",
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: (){
+                            if(controller.text.isNotEmpty){
+                              Navigator.pop(context);
+                              setState(() {
+                                /// copy the new value or existing value of duration
+                                widget.workout!.exercises![widget.exIndex!]
+                                = widget.workout!.exercises![widget.exIndex!].copyWith(
+                                    duration: int.parse(controller.text)
+                                );
+                                /// store all values into cubit
+                                BlocProvider.of<WorkoutsCubit>(context).saveWorkout(widget.workout!, widget.index);
+                              });
+                            }
+                          },
+                          child: const Text("Save"),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                child: NumberPicker(
+                  itemHeight: 30.0,
+                  value: widget.workout!.exercises![widget.exIndex!].duration!,
+                  minValue: 0,
+                  maxValue: 3599,
+                  textMapper: (strVal)=>formatTime(int.parse(strVal), false),
+                  onChanged: (value)=>setState(() {
+                    /// copy the new value or existing value of duration
+                    widget.workout!.exercises![widget.exIndex!]
+                    = widget.workout!.exercises![widget.exIndex!].copyWith(
+                        duration: value
+                    );
+                    /// store all values into cubit
+                    BlocProvider.of<WorkoutsCubit>(context).saveWorkout(widget.workout!, widget.index);
+                  }),
+                ),
               ),
             ),
           ],
